18c6fec7eb757d0a6e2b1530a6196998997f4089
Klepov Yura <onword333@gmail.com>
2019-01-13 02:12:36 +0300

    v2

--- fitnes/dist/scripts/main.js
+++ fitnes/dist/scripts/main.js
@@ -5,26 +5,26 @@ $('#doOrder').click(function() {
 
 $('#toLearnMore').click(function(e){
   e.preventDefault();
-  
+
   var
     sc = $(this).attr("href"),
     dn = $(sc).offset().top;
-    
+
   $('html, body').animate({scrollTop: dn}, 1000);
 });
 
 
 $('#formOrder').submit(function(e) {
-  var 
+  var
     elem = $(this),
     button = $("[type=submit], button",elem);
-  
+
   var name = $('input[name=name]', this);
   var phone = $('input[name=phone]', this);
-  
+
   name.val($.trim(name.val()));
   phone.val($.trim(phone.val()));
-  
+
   if (!name.val()) {
     alert('Укажите корректные ФИО!');
     return false;
@@ -45,13 +45,13 @@ $(document).ready(function() {
 });
 
 function carousel() {
-  var i;
-  var header_el = document.getElementsByClassName("header header_bg color-overlay");
-//  for (i = 0; i < x.length; i++) {
-//    x[i].style.display = "none";
-//  }
-  slideIndex++;
-  if (slideIndex > 3) {slideIndex = 1}
-  header_el[0].style.backgroundImage = "url(../images/mini_bands_" + slideIndex + ".jpg)";
-  setTimeout(carousel, 3000); // Change image every 2 seconds
+//   var i;
+//   var header_el = document.getElementsByClassName("header header_bg color-overlay");
+// //  for (i = 0; i < x.length; i++) {
+// //    x[i].style.display = "none";
+// //  }
+//   slideIndex++;
+//   if (slideIndex > 3) {slideIndex = 1}
+//   header_el[0].style.backgroundImage = "url(../images/mini_bands_" + slideIndex + ".jpg)";
+//   setTimeout(carousel, 3000); // Change image every 2 seconds
 }
